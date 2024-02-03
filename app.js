316adb78b453d91eac6a382b319c58aa23a937c7
Sabona Abubeker <112669369+sabona35@users.noreply.github.com>
2024-02-03 12:26:59 -0700

    Initial commit

---
+++ app.js
@@ -0,0 +1,27 @@
+
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+
+app.use(bodyParser.text({ type: '*/*' }));
+
+app.post('/', function(req, res){
+    //get the accept so we can set response accept type
+    var accept = req.get('Content-Type');
+
+    //get the body to send back
+    var body = req.body
+
+    if(req.get('Content-Type')) {
+        console.log(req.get('Content-Type'))
+        res = res.type (req.get('Content-Type'));
+    }
+    res.send (body);
+});
+
+app.listen(process.env.PORT || 8008, function(){
+    console.log('Listening on port %d', this.address().port);
+});
+module.exports = app; // for testing
+
+//curl -d "echo" -H "Content-Type: text" -X POST http://localhost:8008
